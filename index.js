b1dc1e5aa84b2b3c634661ce317d3c6ffa63ca6f
pershiba-velusamy06 <pershiba@elred.io>
2023-10-13 17:04:37 +0530

    index

--- index.js
+++ index.js
@@ -1,8 +1,27 @@
-const BuiltTime = require('./built-time');
-module.exports = (request, response) => {
-  response.setHeader('content-type', 'text/plain');
-  response.send(`
-    This Serverless Function was built at ${new Date(BuiltTime)}.
-    The current time is ${new Date()}
-  `);
-};
+const bodyParser = require('body-parser');
+const express = require('express');
+
+const eventRoutes = require('./routes/events');
+const authRoutes = require('./routes/auth');
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PATCH,DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type,Authorization');
+  next();
+});
+
+app.use(authRoutes);
+
+app.use('/events', eventRoutes);
+
+app.use((error, req, res, next) => {
+  const status = error.status || 500;
+  const message = error.message || 'Something went wrong.';
+  res.status(status).json({ message: message });
+});
+
+app.listen(8080);
